6b02502aa04ad934db278531df3b5e9c01d5b416
trinh7293 <trinh7293@gmail.com>
2018-08-10 15:28:09 +0700

    sucessfully dump data

--- test_bulk.py
+++ test_bulk.py
@@ -3,34 +3,54 @@ from elasticsearch.helpers import bulk, streaming_bulk
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 import os
 
-for file in os.listdir("datatest"):
-    print(type(file))
+# for file in os.listdir("datatest"):
+#     print(type(file))
 def gendata():
     # mywords = ['foo', 'bar', 'baz']
-    for file in os.listdir("datatest"):
-        f = open(file, "r")
-        contentMod = f.read().split('\n')
-        yield {
-            "_index": "baiviettest",
-            "_type": "document",
-            "doc": {
-                'title': contentMod[0],
-                'sum'
-                'content'
-                'time'
-                'category'
-                'tags'
-                'source'
-            },
-        }
-# f=open("datatest/11111112.txt", "r")
-# content = f.read()
-# contentMod = content.split('\n')
-# print(len(contentMod))
-# print(content)
+    for filename in os.listdir("data"):
+        with open(os.path.join('data', filename)) as f:
+            contentMod = f.read().split('\n')
+            yield {
+                "_index": "baivietbaomoi",
+                "_type": "baiviet",
+                "doc": {
+                    'title': contentMod[0],
+                    'sum': contentMod[1],
+                    'content': contentMod[2],
+                    'time': contentMod[3],
+                    'category': contentMod[4],
+                    'tags': contentMod[5].split(','),
+                    'source': contentMod[6],
+                },
+            }
+def testdata():
+    # mywords = ['foo', 'bar', 'baz']
+    for filename in os.listdir("data"):
+        with open(os.path.join('data', filename)) as f:
+            content = f.read()
+            contentMod = content.split('\n')
+            if len(contentMod) == 10:
+                print(content)
+                print(filename)
 
-# res = es.search(index="mywords", body={"query": {"match_all": {}}})
+def fillter_data():
+    for filename in os.listdir("data"):
+        with open(os.path.join('data', filename)) as f:
+            content = f.read()
+            contentMod = content.split('\n')
+            if len(contentMod) != 7:
+                print(filename)
+                print(len(contentMod))
+                os.remove(os.path.join('data', filename))
+                # print(content)
+                # print(filename)
+def count_file():
+    DIR = 'data/'
+    fileCount = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+    print(fileCount)
+# fillter_data()
+# count_file()
+bulk(es, gendata())
+# res = es.search(index="baiviettest", body={"query": {"match_all": {}}})
 # for hit in res['hits']['hits']:
 #     print( hit["_source"])
-
-# bulk(es, gendata())
--- test_read_file.py
+++ test_read_file.py
@@ -1,10 +1,5 @@
 import os
 
-f=open("datatest/11111112.txt", "r")
-content = f.read()
-contentMod = content.split('\n')
-# print(len(contentMod))
-tags = contentMod[-2]
-tagsMod = tags.
-print(contentMod[-2])
-print(tagsMod)
+DIR = 'data/'
+fileCount = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+print(fileCount)
