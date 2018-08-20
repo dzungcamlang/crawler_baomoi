7bad0e3f4f2e0902d3bdd6632b8be3e30b8764c2
trinh7293 <trinh7293@gmail.com>
2018-08-20 14:14:09 +0700

    complete tfidf

--- query.py
+++ query.py
@@ -1,62 +1,101 @@
-def search_to_file(term):
-    es = Elasticsearch([{'host': '206.189.92.171', 'port': 9200}])
+from test_tf_idf import cal_tf_idf_dict
+from elasticsearch import Elasticsearch
+
+
+def append_tags_for_post(category_dict, post_source):
+    category_adding = post_source['category']
+    tags = post_source['tags']
+    if category_adding in category_dict:
+        category_dict[category_adding].extend(tags)
+
+    return category_dict
+
+
+def find_category_dict():
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
     # Initialize the scroll
     page = es.search(
         request_timeout=30,
-        index='ospg_post_*',
-        doc_type='post',
+        index='baivietbaomoi2',
+        # doc_type='post',
         scroll='2m',
-        size=5000,
-        body={
-            "query": {
-                "bool": {
-                    "filter": [
-                        {
-                            "query_string": {
-                                "fields": [
-                                    "message",
-                                    "content"
-                                ],
-                                "query": "\"{}\"".format(term)
-                            }
-                        },
-                        {
-                            "query_string": {
-                                "fields": [
-                                    "docType"
-                                ],
-                                "query": "user_post"
-                            }
-                        }
-                    ],
-                    "must_not": [],
-                    "should": [],
-                    "minimum_should_match": 0
-                }
-            }
-        })
-
+        size=50,
+        # body={
+        #     "query": {
+        #         "bool": {
+        #             "filter": [
+        #                 {
+        #                     "query_string": {
+        #                         "fields": [
+        #                             "message",
+        #                             "content"
+        #                         ],
+        #                         "query": "\"{}\"".format(term)
+        #                     }
+        #                 },
+        #                 {
+        #                     "query_string": {
+        #                         "fields": [
+        #                             "docType"
+        #                         ],
+        #                         "query": "user_post"
+        #                     }
+        #                 }
+        #             ],
+        #             "must_not": [],
+        #             "should": [],
+        #             "minimum_should_match": 0
+        #         }
+        #     }
+        # }
+    )
+    hits = page['hits']['hits']
+    # for post in hits:
+    #     print(post['category'])
     sid = page['_scroll_id']
     hits = page['hits']['hits']
     result = []
+
+    category_dict = {
+        "Xã hội": [],
+        "Thế giới": [],
+        "Văn hóa": [],
+        "Kinh tế": [],
+        "Giáo dục": [],
+        "Pháp luật": [],
+        "Thể thao": [],
+        "Giải trí": []
+    }
     for post in hits:
-        result.append(post['source']['userid'] + '' + post['_source']['docId'])
-        print(post['source']['userid'] + '' + post['_source']['docId'])
-        print(len(result))
+        category_dict = append_tags_for_post(category_dict, post['_source'])
+        # result.append(post['source']['userid'] + '' + post['_source']['docId'])
+        # print(post['source']['userid'] + '' + post['_source']['docId'])
+        # print(len(result))
     scroll_size = page['hits']['total']  # Start scrolling
-    while scroll_size > 0:
+    count = 100
+    while count > 0:
+        count -= 1
         print('scroll size', scroll_size)
         print("Scrolling...")
         page = es.scroll(scroll_id=sid, scroll='2m')
         # Update the scroll ID
         sid = page['_scroll_id']
         # Get the number of results that we returned in the last scroll
-        scroll_size = len(page['hits']['hits'])
+        # scroll_size = len(page['hits']['hits'])
         hits = page['hits']['hits']
         for post in hits:
-            result.append(post['source']['userid'] +
-                          '' + post['_source']['docId'])
-            print(post['source']['userid'] + '' + post['_source']['docId'])
-            print(len(result))
+            category_dict = append_tags_for_post(
+                category_dict, post['_source'])
+            # result.append(post['source']['userid'] +
+            #               '' + post['_source']['docId'])
+            # print(post['source']['userid'] + '' + post['_source']['docId'])
+            # print(len(result))
     # Do something with the obtained page
-    file_io.write_new_file('data/post_id/' + term.replace(' ', '_'), result)
+    # file_io.write_new_file('data/post_id/' + term.replace(' ', '_'), result)
+    return category_dict
+
+
+if __name__ == '__main__':
+    category_dict = find_category_dict()
+    # print(category_dict)
+    cal_tf_idf_dict(category_dict)
--- test_tf_idf.py
+++ test_tf_idf.py
@@ -11,21 +11,27 @@ def n_containing(word, bloblist):
 
 
 def idf(word, bloblist):
-    return math.log(len(bloblist) / (n_containing(word, bloblist)))
+    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
 
 
 def tfidf(word, blob, bloblist):
     return tf(word, blob) * idf(word, bloblist)
 
-if __name__ == '__main__':
-    document1 = ['ahohoho', 'ahihi', 'ahuhu', 'ahohoho', 'ahehe']
-    document2 = ['ahihi', 'ahuhu', 'ahihi', 'ahohoho', 'ahihi']
-    document3 = ['ahuhu', 'ahohoho', 'ahuhu', 'ahohoho', 'ahuhu']
-    bloblist = [document1, document2, document3]
-    for i, blob in enumerate(bloblist):
-        print("Top words in category {}".format(i + 1))
-        scores = {word: tfidf(word, blob, bloblist) for word in blob}
-        sorted_words = sorted(
-            scores.items(), key=lambda x: x[1], reverse=True)
-        for word, score in sorted_words[:3]:
-            print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+# if __name__ == '__main__':
+#     document1 = ['ahohoho', 'ahihi', 'ahuhu', 'ahohoho', 'ahehe']
+#     document2 = ['ahihi', 'ahuhu', 'ahihi', 'ahohoho', 'ahihi']
+#     document3 = ['ahuhu', 'ahohoho', 'ahuhu', 'ahohoho', 'ahuhu']
+#     bloblist = [document1, document2, document3]
+
+
+def cal_tf_idf_dict(blobdict):
+    with open('result_tfidf.txt', 'w') as f:
+        for blob, blob_value in blobdict.items():
+            f.write("\n\nTop words in category {}".format(blob))
+            scores = {word: tfidf(word, blob_value, blobdict.items())
+                      for word in blob_value}
+            sorted_words = sorted(
+                scores.items(), key=lambda x: x[1], reverse=True)
+            for word, score in sorted_words[:20]:
+                f.write("\nWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+                # f.write(str(content))
